7145cc23e64f6d217a944418d3edaec07f181f75
Lucas Reis <luk.wik40@gmail.com>
2023-04-23 19:26:04 -0300

    Configurando máscara monetária

--- js/validacao.js
+++ js/validacao.js
@@ -57,6 +57,9 @@ const mensagemDeErro = {
   estado: {
     valueMissing: "O campo de estado não pode estar vazio",
   },
+  preco: {
+    valueMissing: "O campo de preço não pode estar vazio",
+  },
 };
 
 const validadores = {
